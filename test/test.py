95739f57538e9feff41b857f20df0945765e9425
Kevin Guan <kevinwguan@protonmail.com>
2024-04-19 02:36:40 +0000

    tb updates

--- test/test.py
+++ test/test.py
@@ -1,40 +1,29 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: MIT
+#In the SAP-1 computer design, there are no designated inputs for operations. 
+#Instead, the functionality is driven by the instruction set stored in the internal memory. 
+#Once the clock is toggled, the instruction at the current memory address is executed, 
+#and the operation carried out depends on this instruction. 
+#This makes the behavior of the SAP-1 computer inherently dependent on its pre-programmed instructions.
+
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
+
+clocks_per_phase = 10
 
 
 @cocotb.test()
-async def test_project(dut):
-    dut._log.info("Start")
-
-    # Set the clock period to 10 us (100 KHz)
+async def test_rgb_mixer(dut):
+    dut._log.info("start")
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
-    # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
+    # reset
+    dut._log.info("reset")
     dut.rst_n.value = 0
+
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 128)
+    
 
-    dut._log.info("Test project behavior")
-
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
